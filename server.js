c83c609c3b7b49b93c65bbf689b836ffdcfff17a
wwwworkingprocess <wwworkingprocess@gmail.com>
2023-11-23 14:15:35 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "https://restart.fixhd.tv",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+	
+	console.log("NEW CONNECTION ", socket.id)
+
+	socket.on("disconnect", () => {
+		console.log("DISCONNECTED", socket.id)
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		console.log("CALL INIT", socket.id, "->", data.userToCall)
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		console.log("CALL STARTED", socket.id, "->", data.to)
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+server.listen(5000, () => console.log("Server is running on port 5000"))
